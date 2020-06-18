9fac620c322ae70507add9a0b94b725de652e545
ziyadbarhoum <zybarhoum@gmail.com>
2020-06-18 15:27:37 +0300

    initial

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hi");
+
+const cells = document.querySelectorAll('td');
+for( var i = 0; i < cells.length; i++){
+    cells[i].addEventListener('click',function(){
+        return X;
+    })
+};
